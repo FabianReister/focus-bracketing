20effe4c89f7a59a96fc1f712c20c19814c1d247
Fabian Reister <fabian.reister92@gmail.com>
2019-08-06 21:28:25 +0200

    Cleanup

--- BracketingRunner.py
+++ BracketingRunner.py
@@ -1,25 +1,18 @@
 import gphoto2 as gp
 import logging
 import os
+from numbers import Number
 
-# https://github.com/jim-easterbrook/python-gphoto2/blob/master/examples/cam-conf-view-gui.py
-def get_camera_model(camera_config):
-    # get the camera model
-    OK, camera_model = gp.gp_widget_get_child_by_name(
-        camera_config, 'cameramodel')
-    if OK < gp.GP_OK:
-        OK, camera_model = gp.gp_widget_get_child_by_name(
-            camera_config, 'model')
-    if OK >= gp.GP_OK:
-        camera_model = camera_model.get_value()
-    else:
-        camera_model = ''
-    return camera_model
 
 class BracketingRunner(object):
+    camera: gp.Camera = None
+    context: gp.Context = None
 
-    context = None
-    camera = None
+    "Number of images that will be created"
+    n_images: int = 20
+
+    "List of images created during focus bracketing. They are stored on the camera and can be downloaded."
+    file_paths = list()
 
     def __init__(self):
         self.connect()
@@ -28,51 +21,32 @@ class BracketingRunner(object):
     def connect(self):
         logging.info("Connecting to camera")
         self.context = gp.gp_context_new()
-        #self.camera = gp.check_result(gp.gp_camera_new())
         self.camera = gp.Camera()
         self.camera.init()
 
-        camera_config = self.camera.get_config()
-        camera_model = get_camera_model(camera_config)
-        #gp.put_camera_capture_preview_mirror(self.camera, camera_config, camera_model)
-
-        #gp.check_result(gp.gp_camera_init(self.camera, self.context))
-        #text = gp.check_result(gp.gp_camera_get_summary(self.camera, self.context))
-
-        #print(text)
-
     def configure(self):
         """
-        Sets the camera parameters such that the focus bracketing can be executed
+        Sets the camera parameters such that the focus bracketing can be performed
 
         this includes
         - preview mode (flips the mirror)
         - manual focus
 
-        Furthermore, the camera stores the images directly on the memory card to reduce latency. 
+        Furthermore, the camera will store the images directly on the memory card to reduce latency.
 
         """
 
         logging.info("Configuring camera")
-        #self.config = gp.check_result(gp.gp_camera_get_config(self.camera, self.context))
-
-        # set-config /main/actions/viewfinder=1
-        #viewfinder_config = gp.check_result(gp.gp_widget_get_child_by_name(self.config, 'viewfinder'))
-        #gp.gp_widget_set_value(viewfinder_config, 1)
-
         cfg = self.camera.get_config()
 
         viewfinder_config = cfg.get_child_by_name('viewfinder')
         viewfinder_config.set_value(1)
 
-        # gp.gp_camera_capture_preview(self.camera, self.context)
-        #
         # set-config /main/capturesettings/focusmode2=MF
         focus_mode_config = cfg.get_child_by_name('focusmode2')
-        #TODO focus_mode_config.set_value("MF")
-        #
-        #
-        # # set-config /main/capturesettings/liveviewaffocus=Single-servo AF
+        # TODO focus_mode_config.set_value("MF")
+
+        # set-config /main/capturesettings/liveviewaffocus=Single-servo AF
         focus_config = cfg.get_child_by_name('liveviewaffocus')
         focus_config.set_value("Single-servo AF")
 
@@ -80,31 +54,44 @@ class BracketingRunner(object):
         capture_target = cfg.get_child_by_name('capturetarget')
         capture_target.set_value("Memory card")
 
-
         self.camera.set_config(cfg)
 
+    def perform_focus_step(self, step=50):
+        """
+        The focus is shifted by applying a fixed number of focus steps.
 
-    def perform_bracketing(self):
+        :param step:
+        :return:
+        """
 
-        logging.info("Performing bracketing")
+        assert isinstance(step, Number)
+
+        cfg = self.camera.get_config()
         # set-config /main/actions/manualfocusdrive 10
+        focus_drive_config = cfg.get_child_by_name('manualfocusdrive')
+        focus_drive_config.set_value(step)
+        self.camera.set_config(cfg)
 
+    def perform_focus_bracketing(self):
+        logging.info("Performing focus bracketing")
 
+        self.file_paths.clear()
 
-        for i in range(20):
-            cfg = self.camera.get_config()
-            focus_drive_config = cfg.get_child_by_name('manualfocusdrive')
-            focus_drive_config.set_value(50)
-            self.camera.set_config(cfg)
-
+        for _ in range(self.n_images):
+            self.perform_focus_step()
             file_path = gp.check_result(gp.gp_camera_capture(self.camera, gp.GP_CAPTURE_IMAGE, self.context))
+            self.file_paths.append(file_path)
 
-            print('Camera file path: {0}/{1}'.format(file_path.folder, file_path.name))
-            target = os.path.join('/tmp', file_path.name)
-            print('Copying image to', target)
-            #camera_file = gp.check_result(gp.gp_camera_file_get(
-            #    self.camera, file_path.folder, file_path.name, gp.GP_FILE_TYPE_NORMAL))
-            #gp.check_result(gp.gp_file_save(camera_file, target))
+    def download_images(self, path='/tmp'):
+        for file_path in self.file_paths:
+            # print('Camera file path: {0}/{1}'.format(file_path.folder, file_path.name))
+            target = os.path.join(path, file_path.name)
+            logging.info('Copying image to', target)
+            camera_file = gp.check_result(gp.gp_camera_file_get(
+                self.camera, file_path.folder, file_path.name, gp.GP_FILE_TYPE_NORMAL))
+            gp.check_result(gp.gp_file_save(camera_file, target))
+
+        self.file_paths.clear()
 
     def __del__(self):
         logging.info("Closing camera")
@@ -116,4 +103,5 @@ if __name__ == "__main__":
     callback_obj = gp.check_result(gp.use_python_logging())
 
     bracketing = BracketingRunner()
-    bracketing.perform_bracketing()
+    bracketing.perform_focus_bracketing()
+    bracketing.download_images()
